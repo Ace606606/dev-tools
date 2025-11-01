dd2b3bb6843f2159e991cb0e9b7fccfd6a3f073b
g.ryakin <g.ryakin@tetra-soft.ru>
2025-11-01 11:42:03 +0300

    upd

---
+++ build.sh
@@ -0,0 +1,2 @@
+g++ -std=c++17 -O3 ./scripts/cpp/src/vscode_config_manager/vscode_config_manager.cpp -o ./scripts/cpp/dst/vscode_config_manager
+g++ -std=c++17 -O3 ./scripts/cpp/src/print_directory_structure/print_directory_structure.cpp -o ./scripts/cpp/dst/print_directory_structure
