0705dd92418ef61372b240508dc2dfca1cbbed8b
g.ryakin <g.ryakin@tetra-soft.ru>
2025-11-01 17:57:36 +0300

    upd

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++ -std=c++17 -O3 ./scripts/cpp/src/vscode_config_manager/vscode_config_manager.cpp -o ./scripts/cpp/dst/vscode_config_manager
-g++ -std=c++17 -O3 ./scripts/cpp/src/print_directory_structure/print_directory_structure.cpp -o ./scripts/cpp/dst/print_directory_structure
+g++ -std=c++17 -Wall -Wextra -Werror -O3 ./scripts/cpp/src/vscode_config_manager/vscode_config_manager.cpp -o ./scripts/cpp/dst/vscode_config_manager
+g++ -std=c++17 -Wall -Wextra -Werror -O3 ./scripts/cpp/src/print_directory_structure/print_directory_structure.cpp -o ./scripts/cpp/dst/print_directory_structure
