0eb4fbf918a02b66685356d693196a36dcba3592
ace <r.grisha.95@mail.ru>
2025-11-25 16:53:08 +0300

    upd

---
+++ test/python/test.py
@@ -0,0 +1,9 @@
+from test2 import func
+
+def main():
+    print("Run test")
+    result: int = func(1, 2)
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ test/python/test2.py
@@ -0,0 +1,5 @@
+def func(x: int, y: int) -> int:
+
+    z: int = x + y
+
+    return z
