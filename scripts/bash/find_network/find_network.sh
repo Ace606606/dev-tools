919179fe5b6c5c9cadeb592e363acec11512783c
ace <r.grisha.95@mail.ru>
2026-01-31 18:19:45 +0300

    update syntax

--- scripts/bash/find_network/find_network.sh
+++ scripts/bash/find_network/find_network.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2  ]; then
+if [ $# -ne 2 ]; then
 	echo "Usage: $0 <IP> <MASK>"
 fi
 
 IP=$1
 MASK=$2
 
-IFS=. read -r i1 i2 i3 i4 <<< "$IP"
-IFS=. read -r m1 m2 m3 m4 <<< "$MASK"
+IFS=. read -r i1 i2 i3 i4 <<<"$IP"
+IFS=. read -r m1 m2 m3 m4 <<<"$MASK"
 
 network=$((i1 & m1)).$((i2 & m2)).$((i3 & m3)).$((i4 & m4))
 
