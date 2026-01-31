8c13d92afa1534cc16f55d91395ca5841d6f71fc
ace <r.grisha.95@mail.ru>
2026-01-31 18:08:32 +0300

    Добавлены санитайзер, vargrind, sanboxs

--- scripts/cpp/src/print_directory_structure/print_directory_structure.cpp
+++ scripts/cpp/src/print_directory_structure/print_directory_structure.cpp
@@ -6,57 +6,68 @@
 
 namespace fs = std::filesystem;
 
-void print_directory_structures(const fs::path& directory, std::vector<std::string>& dirs_to_ignore, int level = 0) {
-    // indentation
-    std::string indent(level * 3, ' ');
+void print_directory_structures( const fs::path& directory, std::vector< std::string >& dirs_to_ignore, int level = 0 )
+{
+     // indentation
+     std::string indent( level * 3, ' ' );
 
-    try {
-        // recursive directory traversal
-        for (const auto& entry : fs::directory_iterator(directory)) {
-            // name element output
-            std::cout << indent << (entry.is_directory() ? "└──/" : "├── ") << entry.path().filename().string() << '\n';
+     try
+     {
+          // recursive directory traversal
+          for( const auto& entry : fs::directory_iterator( directory ) )
+          {
+               // name element output
+               std::cout << indent << ( entry.is_directory() ? "└──/" : "├── " ) << entry.path().filename().string() << '\n';
 
-            // if it is a directory - recursive process its contents
-            if (entry.is_directory() && [&entry, &dirs_to_ignore]() -> bool {
-                    for (const auto& dir : dirs_to_ignore)
-                        if (entry.path().filename().string() == dir) return false;
-                    return true;
-                }()) {
-                print_directory_structures(entry.path(), dirs_to_ignore, level + 1);
-            }
-        }
-    } catch (const fs::filesystem_error& e) {
-        std::cerr << "Error accessing " << directory << ": " << e.what() << std::endl;
-    }
+               // if it is a directory - recursive process its contents
+               if( entry.is_directory() && [ &entry, &dirs_to_ignore ]() -> bool
+                    {
+                         for( const auto& dir : dirs_to_ignore )
+                              if( entry.path().filename().string() == dir ) return false;
+                         return true;
+                    }() )
+               {
+                    print_directory_structures( entry.path(), dirs_to_ignore, level + 1 );
+               }
+          }
+     }
+     catch( const fs::filesystem_error& e )
+     {
+          std::cerr << "Error accessing " << directory << ": " << e.what() << std::endl;
+     }
 }
 
-std::vector<std::string> forming_dirs_to_ignor(const std::string& input_dirs_ignore) {
-    std::vector<std::string> dirs_to_ignore;
-    std::istringstream iss(input_dirs_ignore);
-    std::string token;
-    while (iss >> token) dirs_to_ignore.push_back(token);
-    return dirs_to_ignore;
+std::vector< std::string > forming_dirs_to_ignor( const std::string& input_dirs_ignore )
+{
+     std::vector< std::string > dirs_to_ignore;
+     std::istringstream iss( input_dirs_ignore );
+     std::string token;
+     while( iss >> token )
+          dirs_to_ignore.push_back( token );
+     return dirs_to_ignore;
 }
 
-int main() {
-    std::string path;
-    std::cout << "Enter the path to the directory: ";
-    std::getline(std::cin, path);
+int main()
+{
+     std::string path;
+     std::cout << "Enter the path to the directory: ";
+     std::getline( std::cin, path );
 
-    if (!fs::exists(path)) {
-        std::cerr << "Error: directory does not exist!" << std::endl;
-        return 1;
-    }
+     if( !fs::exists( path ) )
+     {
+          std::cerr << "Error: directory does not exist!" << std::endl;
+          return 1;
+     }
 
-    std::vector<std::string> dirs_to_ignore;
-    std::string input_dirs_ignore;
-    std::cout << "Enter directories to ignore (space separated): ";
-    std::getline(std::cin, input_dirs_ignore);
+     std::vector< std::string > dirs_to_ignore;
+     std::string input_dirs_ignore;
+     std::cout << "Enter directories to ignore (space separated): ";
+     std::getline( std::cin, input_dirs_ignore );
 
-    if (input_dirs_ignore.size()) dirs_to_ignore = forming_dirs_to_ignor(input_dirs_ignore);
+     if( input_dirs_ignore.size() ) dirs_to_ignore = forming_dirs_to_ignor( input_dirs_ignore );
 
-    std::cout << "Directory structure " << path << ":\n";
-    print_directory_structures(path, dirs_to_ignore);
+     std::cout << "Directory structure " << path << ":\n";
+     print_directory_structures( path, dirs_to_ignore );
 
-    return 0;
+     return 0;
 }
--- scripts/cpp/src/vscode_config_manager/vscode_config_manager.cpp
+++ scripts/cpp/src/vscode_config_manager/vscode_config_manager.cpp
@@ -3,39 +3,47 @@
 #include <iostream>
 #include <string>
 
-void copyVSCodeConfig(const std::filesystem::path& source_path) {
-    const std::filesystem::path target_path = std::filesystem::current_path() / ".vscode";
+void copyVSCodeConfig( const std::filesystem::path& source_path )
+{
+     const std::filesystem::path target_path = std::filesystem::current_path() / ".vscode";
 
-    try {
-        if (std::filesystem::exists(target_path)) {
-            std::filesystem::remove_all(target_path);
-            std::cout << "[INFO] old .vscode folder remove\n";
-        }
+     try
+     {
+          if( std::filesystem::exists( target_path ) )
+          {
+               std::filesystem::remove_all( target_path );
+               std::cout << "[INFO] old .vscode folder remove\n";
+          }
 
-        std::filesystem::copy(source_path, target_path, std::filesystem::copy_options::recursive);
-        std::cout << "[SUCCESS] Config copied from " << source_path << "\n";
-    } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "[ERROR] " << e.what() << '\n';
-    }
+          std::filesystem::copy( source_path, target_path, std::filesystem::copy_options::recursive );
+          std::cout << "[SUCCESS] Config copied from " << source_path << "\n";
+     }
+     catch( const std::filesystem::filesystem_error& e )
+     {
+          std::cerr << "[ERROR] " << e.what() << '\n';
+     }
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2 || strcmp(argv[1], "create") != 0) {
-        std::cerr << "Usage: " << argv[0] << " create [path_to_vscode_folder]\n";
-        std::cerr << "Default path: ~/.config/Code/User/.vscode\n";
-        return 1;
-    }
+int main( int argc, char* argv[] )
+{
+     if( argc < 2 || strcmp( argv[ 1 ], "create" ) != 0 )
+     {
+          std::cerr << "Usage: " << argv[ 0 ] << " create [path_to_vscode_folder]\n";
+          std::cerr << "Default path: ~/.config/Code/User/.vscode\n";
+          return 1;
+     }
 
-    std::filesystem::path default_path =
-        std::filesystem::path(getenv("HOME")) / ".config" / "Code" / "User" / ".vscode";
+     std::filesystem::path default_path =
+          std::filesystem::path( getenv( "HOME" ) ) / ".config" / "Code" / "User" / ".vscode";
 
-    std::filesystem::path source_path = (argc >= 3) ? std::filesystem::path(argv[2]) : default_path;
+     std::filesystem::path source_path = ( argc >= 3 ) ? std::filesystem::path( argv[ 2 ] ) : default_path;
 
-    if (!std::filesystem::exists(source_path)) {
-        std::cerr << "[ERROR] Source folder not found: " << source_path << '\n';
-    }
+     if( !std::filesystem::exists( source_path ) )
+     {
+          std::cerr << "[ERROR] Source folder not found: " << source_path << '\n';
+     }
 
-    copyVSCodeConfig(source_path);
+     copyVSCodeConfig( source_path );
 
-    return 0;
+     return 0;
 }
